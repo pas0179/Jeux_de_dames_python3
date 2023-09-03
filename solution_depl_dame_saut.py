c93ad411b5f8249a37f65c86f75b1ab62595669b
pas0179 <ps.depaninfo@gmail.com>
2023-09-03 17:00:04 +0200

    Code Dame : changement dans : solution_depl_dame_saut.py

--- solution_depl_dame_saut.py
+++ solution_depl_dame_saut.py
@@ -5,7 +5,6 @@ noir ou blanc
 """
 
 from solution_depl_dame_base import SolDeplDames
-
 from fonctions import convert_dict_lst
 
 
@@ -114,20 +113,36 @@ class SautDames:
 
     def fusion_dict(self, dict_1, dict_2):
         # Dict1 : self.dict_saut , dict2 : dict_saut
-        # dict_fusion = {}
         dict_temp = {}
 
-        lst_dict1, lst_dict2 = [], []
+        lst_temp1 = []
+        lst_temp2 = []
 
-        for val in dict_1.values():
-            lst_dict1.extend(val)
+        lst_temp4 = []
 
-        for val in dict_2.values():
-            lst_dict2.extend(val)
+        lst_temp1 = [val for val in dict_1.values()]
+        lst_temp2 = [val for val in dict_2.values()]
 
-        print(lst_dict1)
-        print(lst_dict2)
+        for val in lst_temp2:
+            pos = val[0]
 
+            for v in lst_temp1:
+                if pos in v:
+                    """Recherche de l'index par la valeur dans la liste"""
+                    index_pos = v.index(pos)
+
+                    lst_temp = v[:index_pos] + val
+                    lst_temp4.append(lst_temp)
+
+                else:
+                    continue
+
+        compteur = 0
+
+        """ Création du dictionnaire corrigé avec nouvelles coordonnées """
+        for val in lst_temp4:
+            compteur += 1
+            dict_temp[str(compteur)] = val
 
         return dict_temp
 
@@ -151,7 +166,7 @@ class SautDames:
         deplhg, pn_suphg, pb_suphg = self.depl.solution_depl_dame_h_gauche(position)
         deplhd, pn_suphd, pb_suphd = self.depl.solution_depl_dame_h_droit(position)
 
-        if len(deplbg) > 0 and deplbg not in self.dict_saut.values():
+        if len(deplbg) > 0:
             """
             Test si saut avec methode lst_nouveau_depl
             - retourne une liste des deplacements nettoyés
@@ -163,10 +178,6 @@ class SautDames:
             if len(lst_new_pos) > 0:
                 """Saut possible"""
                 self.cpt += 1
-                dict_saut[self.sol + str(self.cpt)] = [position, *depl]
-                # new_pos[self.sol + str(self.cpt)] = lst_new_pos
-
-                lst_new_pos_saut.extend(lst_new_pos)
 
                 if self.color_dame == "noir":
                     lst_pb_sup.extend(pb_supbg)
@@ -174,11 +185,84 @@ class SautDames:
                     lst_pn_sup.extend(pn_supbg)
 
                 """ Après un deplbg ---> deplbd et deplhg si saut dans deplbg """
+                """ Test si saut en deplbd avec appel de la methode """
+                for pos in lst_new_pos:
+                    """Test si saut en deplbd avec appel de la methode"""
+                    (
+                        deplbd_temp,
+                        pn_sup_temp,
+                        pb_sup_temp,
+                    ) = self.depl.solution_depl_dame_b_droit(pos)
 
+                    if len(deplbd_temp) > 0:
+                        lst_new_pos_temp = self.find_saut(
+                            deplbd_temp, pn_sup_temp, pb_sup_temp
+                        )
+                        depl_temp = self.nettoyage_depl(
+                            deplbd_temp, pn_sup_temp, pb_sup_temp
+                        )
+
+                        if len(lst_new_pos_temp) > 0:
+                            dict_saut[self.sol + str(self.cpt)] = [
+                                position,
+                                pos,
+                                *depl_temp,
+                            ]
+
+                            if self.color_dame == "noir":
+                                lst_pb_sup.extend(pb_sup_temp)
+                            else:
+                                lst_pn_sup.extend(pn_sup_temp)
+
+                            """ Ajout a la liste pour recherche nouveau saut """
+                            lst_new_pos_saut.append(lst_new_pos_temp)
+
+                        else:
+                            pass
+                    else:
+                        pass
+
+                    """ Test si saut en deplhg avec appel de la methode """
+                    (
+                        deplhg_temp,
+                        pn_sup_temp,
+                        pb_sup_temp,
+                    ) = self.depl.solution_depl_dame_h_gauche(pos)
+
+                    if len(deplhg_temp) > 0:
+                        lst_new_pos_temp = self.find_saut(
+                            deplhg_temp, pn_sup_temp, pb_sup_temp
+                        )
+                        depl_temp = self.nettoyage_depl(
+                            deplhg_temp, pn_sup_temp, pb_sup_temp
+                        )
+
+                        if len(lst_new_pos_temp) > 0:
+                            dict_saut[self.sol + str(self.cpt)] = [
+                                position,
+                                pos,
+                                *depl_temp,
+                            ]
+
+                            if self.color_dame == "noir":
+                                lst_pb_sup.extend(pb_sup_temp)
+                            else:
+                                lst_pn_sup.extend(pn_sup_temp)
+
+                            lst_new_pos_saut.append(lst_new_pos_temp)
+                        else:
+                            pass
+                    else:
+                        continue
+
+                if len(dict_saut) == 0:
+                    dict_saut[self.sol + str(self.cpt)] = [position, *depl]
+                else:
+                    pass
             else:
                 lst_depl.extend(depl)
 
-        if len(deplbd) > 0 and deplbd not in self.dict_saut.values():
+        if len(deplbd) > 0:
             """Test si saut avec methode lst_nouveau_depl"""
             lst_new_pos = self.find_saut(deplbd, pn_supbd, pb_supbd)
             depl = self.nettoyage_depl(deplbd, pn_supbd, pb_supbd)
@@ -187,15 +271,86 @@ class SautDames:
                 """Saut possible"""
                 self.cpt += 1
 
-                dict_saut[self.sol + str(self.cpt)] = [position, *depl]
-                # new_pos[self.sol + str(self.cpt)] = lst_new_pos
-
-                lst_new_pos_saut.extend(lst_new_pos)
-
                 if self.color_dame == "noir":
                     lst_pb_sup.extend(pb_supbd)
                 else:
                     lst_pn_sup.extend(pn_supbd)
+
+                """ Après un deplbd ---> deplbg et deplhd si saut dans deplbd """
+                """ Test si saut en deplbd avec appel de la methode """
+                for pos in lst_new_pos:
+                    """Test si saut en deplhd avec appel de la methode"""
+                    (
+                        deplhd_temp,
+                        pn_sup_temp,
+                        pb_sup_temp,
+                    ) = self.depl.solution_depl_dame_h_droit(pos)
+
+                    if len(deplhd_temp) > 0:
+                        lst_new_pos_temp = self.find_saut(
+                            deplhd_temp, pn_sup_temp, pb_sup_temp
+                        )
+                        depl_temp = self.nettoyage_depl(
+                            deplhd_temp, pn_sup_temp, pb_sup_temp
+                        )
+
+                        if len(lst_new_pos_temp) > 0:
+                            dict_saut[self.sol + str(self.cpt)] = [
+                                position,
+                                pos,
+                                *depl_temp,
+                            ]
+
+                            if self.color_dame == "noir":
+                                lst_pb_sup.extend(pb_sup_temp)
+                            else:
+                                lst_pn_sup.extend(pn_sup_temp)
+
+                            lst_new_pos_saut.append(lst_new_pos_temp)
+
+                        else:
+                            pass
+                    else:
+                        pass
+
+                    """ Test si saut en deplbg avec appel de la methode """
+                    (
+                        deplbg_temp,
+                        pn_sup_temp,
+                        pb_sup_temp,
+                    ) = self.depl.solution_depl_dame_b_gauche(pos)
+
+                    if len(deplbg_temp) > 0:
+                        lst_new_pos_temp = self.find_saut(
+                            deplbg_temp, pn_sup_temp, pb_sup_temp
+                        )
+                        depl_temp = self.nettoyage_depl(
+                            deplbg_temp, pn_sup_temp, pb_sup_temp
+                        )
+
+                        if len(lst_new_pos_temp) > 0:
+                            dict_saut[self.sol + str(self.cpt)] = [
+                                position,
+                                pos,
+                                *depl_temp,
+                            ]
+
+                            if self.color_dame == "noir":
+                                lst_pb_sup.extend(pb_sup_temp)
+                            else:
+                                lst_pn_sup.extend(pn_sup_temp)
+
+                            lst_new_pos_saut.append(lst_new_pos_temp)
+
+                        else:
+                            pass
+                    else:
+                        continue
+
+                if len(dict_saut) == 0:
+                    dict_saut[self.sol + str(self.cpt)] = [position, *depl]
+                else:
+                    pass
             else:
                 lst_depl.extend(depl)
 
@@ -217,6 +372,82 @@ class SautDames:
                     lst_pb_sup.extend(pb_suphg)
                 else:
                     lst_pn_sup.extend(pn_suphg)
+
+                """ Après un deplhg ---> deplbg et deplhd si saut dans deplhg """
+                """ Test si saut en deplbd avec appel de la methode """
+                for pos in lst_new_pos:
+                    """Test si saut en deplhd avec appel de la methode"""
+                    (
+                        deplhd_temp,
+                        pn_sup_temp,
+                        pb_sup_temp,
+                    ) = self.depl.solution_depl_dame_h_droit(pos)
+
+                    if len(deplhd_temp) > 0:
+                        lst_new_pos_temp = self.find_saut(
+                            deplhd_temp, pn_sup_temp, pb_sup_temp
+                        )
+                        depl_temp = self.nettoyage_depl(
+                            deplhd_temp, pn_sup_temp, pb_sup_temp
+                        )
+
+                        if len(lst_new_pos_temp) > 0:
+                            dict_saut[self.sol + str(self.cpt)] = [
+                                position,
+                                pos,
+                                *depl_temp,
+                            ]
+
+                            if self.color_dame == "noir":
+                                lst_pb_sup.extend(pb_sup_temp)
+                            else:
+                                lst_pn_sup.extend(pn_sup_temp)
+
+                            lst_new_pos_saut.append(lst_new_pos_temp)
+
+                        else:
+                            pass
+                    else:
+                        pass
+
+                    """Test si saut en deplbg avec appel de la methode"""
+                    (
+                        deplbg_temp,
+                        pn_sup_temp,
+                        pb_sup_temp,
+                    ) = self.depl.solution_depl_dame_b_gauche(pos)
+
+                    if len(deplbg_temp) > 0:
+                        lst_new_pos_temp = self.find_saut(
+                            deplbg_temp, pn_sup_temp, pb_sup_temp
+                        )
+                        depl_temp = self.nettoyage_depl(
+                            deplbg_temp, pn_sup_temp, pb_sup_temp
+                        )
+
+                        if len(lst_new_pos_temp) > 0:
+                            dict_saut[self.sol + str(self.cpt)] = [
+                                position,
+                                pos,
+                                *depl_temp,
+                            ]
+
+                            if self.color_dame == "noir":
+                                lst_pb_sup.extend(pb_sup_temp)
+                            else:
+                                lst_pn_sup.extend(pn_sup_temp)
+
+                            lst_new_pos_saut.append(lst_new_pos_temp)
+
+                        else:
+                            pass
+                    else:
+                        continue
+
+                if len(dict_saut) == 0:
+                    dict_saut[self.sol + str(self.cpt)] = [position, *depl]
+                else:
+                    pass
             else:
                 lst_depl.extend(depl)
 
@@ -238,6 +469,81 @@ class SautDames:
                     lst_pb_sup.extend(pb_suphd)
                 else:
                     lst_pn_sup.extend(pn_suphd)
+
+                """ Après un deplhd ---> deplbd et deplhg si saut dans deplhd """
+                """ Test si saut en deplbd avec appel de la methode """
+                for pos in lst_new_pos:
+                    """Test si saut en deplbd avec appel de la methode"""
+                    (
+                        deplbd_temp,
+                        pn_sup_temp,
+                        pb_sup_temp,
+                    ) = self.depl.solution_depl_dame_b_droit(pos)
+
+                    if len(deplbd_temp) > 0:
+                        lst_new_pos_temp = self.find_saut(
+                            deplbd_temp, pn_sup_temp, pb_sup_temp
+                        )
+                        depl_temp = self.nettoyage_depl(
+                            deplbd_temp, pn_sup_temp, pb_sup_temp
+                        )
+
+                        if len(lst_new_pos_temp) > 0:
+                            dict_saut[self.sol + str(self.cpt)] = [
+                                position,
+                                pos,
+                                *depl_temp,
+                            ]
+
+                            if self.color_dame == "noir":
+                                lst_pb_sup.extend(pb_sup_temp)
+                            else:
+                                lst_pn_sup.extend(pn_sup_temp)
+
+                            lst_new_pos_saut.append(lst_new_pos_temp)
+
+                        else:
+                            pass
+
+                    else:
+                        pass
+
+                    """Test si saut en deplhg avec appel de la methode """
+                    (
+                        deplhg_temp,
+                        pn_sup_temp,
+                        pb_sup_temp,
+                    ) = self.depl.solution_depl_dame_h_gauche(pos)
+
+                    if len(deplhg_temp) > 0:
+                        lst_new_pos_temp = self.find_saut(
+                            deplhg_temp, pn_sup_temp, pb_sup_temp
+                        )
+                        depl_temp = self.nettoyage_depl(
+                            deplhg_temp, pn_sup_temp, pb_sup_temp
+                        )
+
+                        if len(lst_new_pos_temp) > 0:
+                            dict_saut[self.sol + str(self.cpt)] = [
+                                position,
+                                pos,
+                                *depl_temp,
+                            ]
+
+                            if self.color_dame == "noir":
+                                lst_pb_sup.extend(pb_sup_temp)
+                            else:
+                                lst_pn_sup.extend(pn_sup_temp)
+
+                            lst_new_pos_saut.append(lst_new_pos_temp)
+                        else:
+                            pass
+                    else:
+                        continue
+                if len(dict_saut) == 0:
+                    dict_saut[self.sol + str(self.cpt)] = [position, *depl]
+                else:
+                    pass
             else:
                 lst_depl.extend(depl)
         else:
@@ -253,74 +559,15 @@ class SautDames:
         """Appel de la methode saut_depl_possible pour recupérer
         les possibilités saut ou deplacements"""
 
-        lst_pn_sup = []
-        lst_pb_sup = []
-
         position = self.pos_dame
 
         dict_saut, new_pos, lst_depl, pn_sup, pb_sup = self.saut_depl_possible(position)
 
-        if len(lst_depl) > 0:
-            self.lst_depl = lst_depl
+        self.dict_saut = dict_saut
+        print(f"self.dict_saut : {self.dict_saut}")
+        self.new_pos = new_pos
+        print(f"self.new_pos : {self.new_pos}")
 
-        if len(dict_saut) > 0 and len(new_pos) > 0:
-            self.dict_saut = dict_saut
-            self.new_pos = new_pos
-
-            lst_pn_sup.extend(pn_sup)
-            lst_pb_sup.extend(pb_sup)
-
-        else:
-            pass
-
-        """ Après la 1er diagonale, la dame peut changer de diagonale
-        que si il y a eu un saut possible sur la 1er diagonale et 
-        que sur la 2eme il y a aussi un autre saut disponible """
-
-
-        if len(new_pos) > 0:
-            for pos in new_pos:
-                (
-                    dict_saut_temp,
-                    pos_temp,
-                    _,
-                    pn_temp,
-                    pb_temp,
-                ) = self.saut_depl_possible(pos)
-
-                if len(dict_saut_temp) > 0:
-                    self.dict_saut2.update(dict_saut_temp)
-                    self.new_pos2.extend(pos_temp)
-
-                    if len(pn_temp) > 0:
-                        lst_pn_sup.extend(pn_temp)
-                    elif len(pb_temp) > 0:
-                        lst_pb_sup.extend(pb_temp)
-                    else:
-                        pass
-                else:
-                    pass
-        else:
-            pass
-
-        if len(self.dict_saut2) > 0:
-            """Mise a jour des clés de dict_saut2"""
-            self.dict_saut2 = self.maj_dict_saut(self.dict_saut2)
-
-            self.dict_saut = self.fusion_dict(self.dict_saut, self.dict_saut2)
-            print(f"self.dict_saut : {self.dict_saut}")
-            self.new_pos = self.new_pos2
-
-        else:
-            pass
-
-        self.lst_pn_sup = lst_pn_sup
-        self.lst_pb_sup = lst_pb_sup
-
-        print(f"dict_saut : {self.dict_saut}")
-        # print(f"new_pos : {self.new_pos}")
-
-        print(f"self.dict_saut2 : {self.dict_saut2}")
-        # print(f"self.dict_saut : {self.dict_saut}")
-        # print(f"self.new_pos2 : {self.new_pos2}")
-        # print(f"self.lst_depl : {self.lst_depl}")
+        self.lst_depl = lst_depl
+        self.lst_pn_sup = pn_sup
+        self.lst_pb_sup = pb_sup
